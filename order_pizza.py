450e658b161273a7da8771b745c96845204bf452
Aaron Pfister <AaronPfister@Aarons-MacBook-Pro.local>
2018-08-11 20:01:11 -0700

    working on seeing/manuevering the menu

--- order_pizza.py
+++ order_pizza.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
+# Using pizzapi package from https://github.com/gamagori/pizzapi
 from pizzapi import *
 
 #helper functions
 
 def get_address(): 
-	print("We need your delivery address to get started.")
+	print("We need your delivery address to get started.\n")
 	street = input("Street: ")
 	city = input("City: ")
 	state = input("State: ")
@@ -14,7 +15,7 @@ def get_address():
 	return address
 
 def get_contact_info(filler):
-	print("Next we need some contact info")
+	print("Next we need some contact info.\n")
 	first = input("First name: ")
 	last = input("Last name: ")
 	email = input("Email: ")
@@ -46,33 +47,41 @@ print(menu.display()) #prints entire menu with codes
 
 # Main Prompt
 
-print("Welcome to my pizza delivery script. \n Orders will be made using Dominos Pizza.")
+print("Welcome to my pizza delivery script.\nOrders will be made using Dominos Pizza.\n")
 
-# retrieves address
+# Retrieves address
 while True:
 	address = get_address()
-	print(address.line1)
-	print(address.line2)
+	print('\n' + address.line1)
+	print(address.line2 + '\n')
 	correct = input("Does this information look correct? (y/n): ")
 	if correct == 'y':
-		print("Awesome, let's continue.")
+		print("\nAwesome, let's continue.\n")
 		break
 
-# retrieves contact information
+# Retrieves contact information
 while True:
 	customer = get_contact_info(address)
-	print(customer.first_name) #add function to cleanly print details
+	print('\n' + customer.first_name + '\n') #add function to cleanly print details
 	correct = input("Does this information look correct? (y/n): ")
 	if correct == 'y':
-		print("Awesome, let's continue.")
+		print("\nAwesome, let's continue.\n")
 		break
-		
+
+# Finds closest store to address
 store = address.closest_store()
+print("The closest store to your address is:\n")
+print(str(store.data) + '\n') #prints some data about the closest store
 
-print('\n' * 5)
-print(store.data) #prints some data about the closest store
-
+# Gets menu and asks if the user wants the whole menu or just search
 menu = store.get_menu()
+while True:
+	menu_choice = input("Type 'see menu' or enter and item to search: ")
+	if menu_choice == 'see menu':
+		print(menu.display()) #prints entire menu with codes
+		break
+	# if menu.search(menu_choice) == '':
+		# print("No results shown.\n")
+	print(str(menu.search(menu_choice)))
 
-print('\n' * 5)
-print(menu.display()) #prints entire menu with codes
+
